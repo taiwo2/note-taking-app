496805b241d5f95d1ef04b8ac7a9dece65c49a2a
taiwo2 <adetonat@ymail.com>
2022-05-27 16:33:02 +0100

    Add sideBar component

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,12 +3,49 @@ import React,{useEffect,useState} from 'react';
 import SideBar from './SideBar';
 import './App.css'
 import Main from './Main';
-
+const mapState = (state) =>({
+  notes: state.notes
+})
 const Home = () => {
- 
+  const [activeNote, setActiveNote] = useState(false);
+  const dispatch = useDispatch()
+  const {notes} = useSelector(mapState);
+
+   const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+
+    dispatch(addNotes(newNote))
+    setActiveNote(newNote.id);
+  };
+  const onUpdateNote = (updatedNote) => {
+  
+    // console.log('dd',updateNotes)
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    dispatch(updateNotes(updatedNotesArr))
+  };
+  const getActiveNote = () => {
+    const noted = notes.find(({ id }) => id === activeNote);
+      return noted
+  };
+
   return (
     <div className='main'>
-      <SideBar />
+      <SideBar
+      activeNote={activeNote}
+      setActiveNote={setActiveNote}
+      notes={notes} onAddNote={onAddNote} />
       <Main 
       />
     </div>
--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -1,16 +1,39 @@
 import React, {useState} from 'react'
 
-const Sidebar = () => {
-
+const Sidebar = ({notes,onAddNote,activeNote,setActiveNote}) => {
+  const [textInput, setTextInput] = useState("");
+  const sortedNotes = notes.filter(note => {
+    if (textInput === ""){
+      return note
+    }else {
+      return note.title.toLowerCase().includes(textInput.toLowerCase())
+    }
+  })
   return (
     <div className="app-sidebar">
       <div className="app-sidebar-header">
-        <input name="search" id="search" className='search' type="text" />
-        <button 
-        >+</button>
+        <input 
+          name="search" 
+          id="search" 
+          className='search' type="text"
+          onChange={(e) =>  setTextInput(e.target.value)} 
+        />
+        <button>+</button>
       </div>
       <div className="app-sidebar-notes">
-        
+      {sortedNotes.map(({ id, title,body,lastModified }, i) => (
+          <div key={id}
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <small className="note-meta">
+                Created{" "}{moment(lastModified).startOf().fromNow()}
+            </small>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
